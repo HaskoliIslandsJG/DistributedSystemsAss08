53337ca659bad374e2a707356f6921baec322170
jeje42 <guidon@live.fr>
2015-10-08 11:16:03 +0000

    timeout with number of tries working

--- readme.txt
+++ readme.txt
@@ -2,6 +2,8 @@ Github repo : https://github.com/jeje42/HI-Distributed-Systems.git
 
 Both cases : cd into the bin directory.
 
+Server : launch rmiregistry
+
 1. Start server using (adjust classpath parameter -cp to let it point to your directory that contains the ds_04 subdirectory that contain the *.class files)
 java -cp ./ -Djava.rmi.server.codebase=file:///./ -Djava.security.policy=../policy.txt Server portNumber
 
--- src/Client.java
+++ src/Client.java
@@ -4,13 +4,68 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.Calendar;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 import requests.ReplyRequest;
 import requests.RequestID;
 import serialisation.SerialisationFramework;
 
 public class Client {
+	private final static SerialisationFramework serialisationFramework = new SerialisationFramework();
+	private final static int maximumTries = 3;
+	
+	private DatagramSocket sendSocket = null, receiveSocket = null;
+	private InetAddress serverAdress;
+	private int serverPort , myPort = 6001, nbTries = 0;
+	
+	private void closeSockets(){
+		if (receiveSocket != null)
+			receiveSocket.close();
+		if (sendSocket != null)
+			sendSocket.close();
+	}
+	
+	private boolean doOperation() throws SocketTimeoutException {
+		try{
+			sendSocket = new DatagramSocket();
+			ReplyRequest replyRequest = new ReplyRequest(0, new RequestID(serverAdress, myPort), 0);
+			byte[] message = serialisationFramework.buildByteMessage(replyRequest);
+			
+			DatagramPacket request = new DatagramPacket(message, message.length, serverAdress, serverPort);
+			System.out.println("Try number " + (nbTries+1) + ".");
+			sendSocket.send(request);
+			
+			//Wait for the answer
+			receiveSocket = new DatagramSocket(myPort);
+			
+			while(true){
+				DatagramPacket repliedDatagram = new DatagramPacket(message, message.length);
+				receiveSocket.setSoTimeout(3000);
+				receiveSocket.receive(repliedDatagram);
+				
+				ReplyRequest requestAnswer = (ReplyRequest)serialisationFramework.readFromByteArray(repliedDatagram.getData());
+				
+				if(requestAnswer.getRequestID().equals(replyRequest.getRequestID())){
+					System.out.println("Reply received !");
+					return true;
+				}
+			}
+			
+		}catch(SocketException e) {
+			System.out.println("Socket: " + e.getMessage());
+			return false;
+		} catch (IOException e) {
+			if(e instanceof SocketTimeoutException){
+				throw (SocketTimeoutException)e;
+			}else{
+				System.out.println("IO: " + e.getMessage());
+				return false;
+			}
+		}finally{
+			closeSockets();
+		}
+	}
 	
 	/**
 	 * Arguments :
@@ -27,49 +82,33 @@ public class Client {
 		}
 		
 		
-		DatagramSocket sendSocket = null, receiveSocket = null;
-		SerialisationFramework serialisationFramework = new SerialisationFramework();
 		
+		Client mCLient = new Client();
+		
+			
 		try {
-			sendSocket = new DatagramSocket();
-			InetAddress aHost = InetAddress.getByName(args[0]);
-			int serverPort = Integer.parseInt(args[1]),
-					myPort = 6001;
-			
-			ReplyRequest replyRequest = new ReplyRequest(0, new RequestID(aHost, myPort), 0);
-			
-			byte[] message = serialisationFramework.buildByteMessage(replyRequest);
-			
-			DatagramPacket request = new DatagramPacket(message, message.length, aHost, serverPort);
-			sendSocket.send(request);
-			
-			//TODO : wait for the answer
-			boolean answer = false;
-			receiveSocket = new DatagramSocket(myPort);
-			
-			while(!answer){
-				DatagramPacket repliedDatagram = new DatagramPacket(message, message.length);
-				receiveSocket.setSoTimeout(3000);
-				receiveSocket.receive(repliedDatagram);
-				
-				ReplyRequest requestAnswer = (ReplyRequest)serialisationFramework.readFromByteArray(repliedDatagram.getData());
-				
-				if(requestAnswer.getRequestID().equals(replyRequest.getRequestID())){
-					System.out.println("Reply received !");
-					answer = true;
-				}
-			}
-			System.out.println("Nothing received !");
-			
-		} catch (SocketException e) {
-			System.out.println("Socket: " + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("IO: " + e.getMessage());
-		} finally {
-			if (receiveSocket != null)
-				receiveSocket.close();
-			if (sendSocket != null)
-				sendSocket.close();
+			mCLient.serverAdress = InetAddress.getByName(args[0]);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
 		}
+		mCLient.serverPort = Integer.parseInt(args[1]);
+		
+		
+		//Try to send the request
+		boolean replyOk = false;
+		while(mCLient.nbTries<maximumTries){
+			try{
+				if(mCLient.doOperation()){
+					replyOk = true;
+					mCLient.nbTries = maximumTries+1;
+				}
+			} catch (SocketTimeoutException ste){
+				mCLient.nbTries++;
+			}
+			
+		}
+		if(!replyOk)	System.out.println("No answer after " + mCLient.nbTries + " tries.");
 	}
 }
